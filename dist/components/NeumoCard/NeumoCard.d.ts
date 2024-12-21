4ac928919b55e9ba6bc965cc3fb0b6208348e9f4
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-21 23:28:19 +0000

    it

--- dist/components/NeumoCard/NeumoCard.d.ts
+++ dist/components/NeumoCard/NeumoCard.d.ts
@@ -1,5 +1,11 @@
 import { LitElement } from 'lit';
 export declare class NeumoCard extends LitElement {
+    bgColor: string;
+    shadowLight: string;
+    shadowDark: string;
+    borderRadius: string;
+    padding: string;
     static styles: import("lit").CSSResult;
     render(): import("lit-html").TemplateResult<1>;
+    private _dynamicStyles;
 }
