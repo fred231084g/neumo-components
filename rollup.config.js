d10e5cc2e307e8ed30486f4ee92864166f5adc06
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-21 23:07:56 +0000

    it

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default [
         sourcemap: true,
       },
       {
-        file: 'dist/index.cjs.js',
+        file: 'dist/index.cjs',
         format: 'cjs',
         sourcemap: true,
       },
