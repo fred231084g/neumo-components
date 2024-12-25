d4bdd8c63f3f403077a0d6f720eaed3c16f5f51e
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-25 12:31:28 +0000

    it

--- dev-server.config.js
+++ dev-server.config.js
@@ -1,14 +1,34 @@
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+
 export default {
-    nodeResolve: true,
-    appIndex: 'fixtures/index.html',
-    rootDir: '.',
-    preserveSymlinks: true,
-    middleware: [
-      function rewriteNodeModules(context, next) {
-        if (context.url.startsWith('/node_modules/')) {
-          context.url = context.url.replace('/node_modules/', '/');
+  rootDir: '.',
+  appIndex: 'fixtures/index.html',
+  nodeResolve: true,
+  preserveSymlinks: true,
+  plugins: [esbuildPlugin({ ts: true })],
+  middleware: [
+    function rewriteNodeModules(context, next) {
+      const mimeTypes = {
+        '.js': 'application/javascript',
+        '.mjs': 'application/javascript',
+        '.css': 'text/css',
+        '.html': 'text/html',
+      };
+
+      // Set correct MIME type for module files
+      if (context.url.includes('/node_modules/')) {
+        const ext = context.url.split('.').pop();
+        if (mimeTypes[`.${ext}`]) {
+          context.response.type = mimeTypes[`.${ext}`];
         }
-        return next();
       }
-    ]
-  };
+
+      return next();
+    }
+  ],
+  mimeTypes: {
+    '**/*.js': 'application/javascript',
+    '**/*.mjs': 'application/javascript',
+    '**/*.ts': 'application/javascript'
+  }
+};
